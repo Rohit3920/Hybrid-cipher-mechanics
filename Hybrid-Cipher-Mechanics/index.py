a828f3b50235ef9985311b63a756dde4515112bf
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2025-03-18 16:42:17 +0530

    Implement all the cipher models and use generated key for logically use random model for encryption and decryption

--- Hybrid-Cipher-Mechanics/index.py
+++ Hybrid-Cipher-Mechanics/index.py
@@ -1,31 +1,59 @@
-import HybridEncoDeco.sPlayfairCipher as sPlayfairCipher
-import HybridEncoDeco.sPolyalphabatic as sPolyalphabatic
-import HybridEncoDeco.tRailFenceTransposition as tRailFenceTransposition
-import HybridEncoDeco.sCeaserCipher as sCeaserCipher
+import time
 
-c2 = sPlayfairCipher.playfair_encrypt("Rohit Nittawadekar", "Rohit")
-d2 = sPlayfairCipher.playfair_decrypt(c2, "Rohit")
+from KeyGen.edKay import generate_keys
+from HybridEncoDeco.sCeaserCipher import *
+from HybridEncoDeco.sMonoalphabatic import *
+from HybridEncoDeco.sPlayfairCipher import *
+from HybridEncoDeco.sPolyalphabatic import *
+from HybridEncoDeco.tRailFenceTransposition import *
 
-print(c2)
-print(d2)
-
-kk = 30
-c1 = tRailFenceTransposition.rail_fence_encrypt("Rohit Nittawadekar", kk)
-d1 = tRailFenceTransposition.rail_fence_decrypt(c1, kk)
-
-print(c1)
-print(d1)
-
-c = sPolyalphabatic.polyalphabetic_encrypt("Rohit Nittawadekar", "raman")
-d = sPolyalphabatic.polyalphabetic_decrypt(c, "raman")
-
-print(c)
-print(d)
+# Encryption
+def encryption(ch, plaintext, eKey) :
+    et = monoalphabetic_encrypt(plaintext, eKey) if ch ==1 else ceaser_encrypt(plaintext, eKey) if ch == 2 else playfair_encrypt(plaintext, eKey) if ch == 3 else  rail_fence_encrypt(plaintext, eKey) if ch == 4 else  polyalphabetic_encrypt(plaintext, eKey) if ch == 5 else ceaser_encrypt(plaintext, eKey)
+    return et
 
 
-c3 = sCeaserCipher.ceaser_encrypt("Rohit Nittawadekar", 3)
-d3 = sCeaserCipher.ceaser_decrypt(c3, 3)
+# decryption
+def decryption(ch, ciphertext, dKey) :
+    dt = monoalphabetic_decrypt(ciphertext, dKey) if ch ==1 else ceaser_decrypt(ciphertext, dKey) if ch == 2 else playfair_decrypt(ciphertext, dKey) if ch == 3 else  rail_fence_decrypt(ciphertext, dKey) if ch == 4 else polyalphabetic_decrypt(ciphertext, dKey) if ch == 5 else ceaser_decrypt(ciphertext, numeric_key)
+    return dt
 
-print(c3)
-print(d3)
-print("Hybrid Cipher Mechanics")
+
+#Key managemenet
+numeric_key, alphabetic_key = generate_keys()
+key1 = int(numeric_key)
+key2 = str(alphabetic_key)
+print(key1)
+print(key2)
+
+def chooseED():
+    today = time.localtime()
+    mint = today.tm_min
+    hr = today.tm_hour
+    wday = today.tm_wday
+    mday = today.tm_mday
+    month = today.tm_mon
+    yr = today.tm_year
+
+    t_val = mint *(hr  + wday + mday + month + yr)
+    mod_val = t_val % 100
+    remain_val = int(t_val / 100)
+    ch = (mod_val + remain_val) % 6
+    return ch
+
+#choose key for perticular cipher model
+def chooseKey(ch) :
+    if ch % 2 == 0 :
+        key = key1
+    else :
+        key = key2
+    return key
+
+
+# try example for any
+pText = "hybrid cipher mechanics"
+encrypted_text = encryption(chooseED(), pText, chooseKey(chooseED()))
+print(f"Encrypted: {encrypted_text}")
+
+decrypted_text = decryption(chooseED(), encrypted_text, chooseKey(chooseED()))
+print(f"decrypted: {decrypted_text}")
